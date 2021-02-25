52aefbc65765f0c21ca2f5efc43ecf64a764dbf7
Dojunggeun <jg.do@snu.ac.kr>
2021-02-25 11:20:45 +0000

    Updates

---
+++ precache-manifest.6d9dc7216ce42970c222ddae872050a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/youtube_link_generator/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6c704f005510a649810f",
+    "url": "/youtube_link_generator/static/js/runtime~main.6c704f00.js"
+  },
+  {
+    "revision": "63bda85ed197303991a2",
+    "url": "/youtube_link_generator/static/js/main.63bda85e.chunk.js"
+  },
+  {
+    "revision": "683a332b71b94de8b27e",
+    "url": "/youtube_link_generator/static/js/1.683a332b.chunk.js"
+  },
+  {
+    "revision": "63bda85ed197303991a2",
+    "url": "/youtube_link_generator/static/css/main.a0258eef.chunk.css"
+  },
+  {
+    "revision": "9a53886a6dabc4625eb9676bc072e09d",
+    "url": "/youtube_link_generator/index.html"
+  }
+];
