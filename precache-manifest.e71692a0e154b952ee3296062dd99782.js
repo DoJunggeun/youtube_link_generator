50a48e3e09aadb23a20b317c3b005fc0c9f9f870
Dojunggeun <jg.do@snu.ac.kr>
2021-03-01 02:02:50 +0000

    Updates

--- precache-manifest.e71692a0e154b952ee3296062dd99782.js
+++ precache-manifest.e71692a0e154b952ee3296062dd99782.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/youtube_link_generator/static/js/runtime~main.6c704f00.js"
   },
   {
-    "revision": "8b1b7a08a8941d1a7e7e",
-    "url": "/youtube_link_generator/static/js/main.8b1b7a08.chunk.js"
+    "revision": "dc9876f009826dfa1a27",
+    "url": "/youtube_link_generator/static/js/main.dc9876f0.chunk.js"
   },
   {
     "revision": "575310fbe24ff051e2e4",
     "url": "/youtube_link_generator/static/js/1.575310fb.chunk.js"
   },
   {
-    "revision": "8b1b7a08a8941d1a7e7e",
-    "url": "/youtube_link_generator/static/css/main.5d2318f7.chunk.css"
+    "revision": "dc9876f009826dfa1a27",
+    "url": "/youtube_link_generator/static/css/main.17a39feb.chunk.css"
   },
   {
-    "revision": "102bc2be1468f34cb770ad5be19b8f15",
+    "revision": "204b3fdb742173c2773193396de999e3",
     "url": "/youtube_link_generator/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/youtube_link_generator/precache-manifest.3bf477187395aec2717af7b38b9703fe.js"
+  "/youtube_link_generator/precache-manifest.e71692a0e154b952ee3296062dd99782.js"
 );
 
 workbox.clientsClaim();
